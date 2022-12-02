d51731fbdaea92352cd18650c45e57e17b56b6e4
Bozhidar Batsov <bozhidar@batsov.dev>
2022-12-02 11:32:41 +0200

    Add chomp

--- ocaml/aoc2022/lib/util.ml
+++ ocaml/aoc2022/lib/util.ml
@@ -1,9 +1,16 @@
+let chomp s =
+  let n = String.length s in
+  if n > 0 && s.[n-1] = '\n' then
+    String.sub s 0 (n-1)
+  else
+    s
+
 let lines file =
   let contents = match file with
   | "-" -> In_channel.input_all In_channel.stdin
   | file -> In_channel.with_open_bin file In_channel.input_all
   in
-  String.split_on_char '\n' contents
+  String.split_on_char '\n' (chomp contents)
 
 let max lst =
   let rec aux n = function
